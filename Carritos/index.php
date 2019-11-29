786f6dd80434577207654cd801c7d8d9a3335ffb
Ricardo Mélida <ricardomelida92@gmail.com>
2019-11-29 20:08:44 -0300

    Se agrega a repositorio cursos que se hacen en Udemy

---
+++ Carritos/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Carrito</title>
+
+    <!--Bootstrap 4-->
+    <!--link rel="stylesheet" href="assets/css/bootstrap.min.css"-->
+    <link rel="stylesheet" href="assets/css/bootstrap-theme.min.css">
+
+
+    <!-- Font awesome-->
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+
+
+    <!--  WaitMe   -->
+    <link rel="stylesheet" href="assets/plugins/waitMe/waitMe.min.css">
+</head>
+<body>
+    <h1>Titulo Modificado </h1>
+    
+
+
+    <!-- Bootstrap 4-->
+    <script src="assets/js/bootstrap.min.js"></script>
+
+    <!-- JQuery 3.4.1 -->
+    <script src="assets/js/jquery-3.4.1.slim.min.js"></script>
+
+    <!--  Popper JS-->
+    <script src="assets/js/popper.min.js"></script>
+
+    <!--  Sweet Alert 2-->
+    <script src="assets/js/sweetalert2.all.js"></script>
+
+    <!--  WaitMe   -->
+    <script src="assets/plugins/waitMe/waitMe.min.js"></script>
+
+    <script>
+      
+    </script>
+</body>
+</html>
