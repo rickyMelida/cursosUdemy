24b96c5ec8a09acbde579908e7f4120855c3dd31
Ricardo Mélida <ricardomelida92@gmail.com>
2019-12-01 18:15:13 -0300

    Se maqueta barra de navegacion y footer

--- Carritos/index.php
+++ Carritos/index.php
@@ -4,33 +4,96 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Carrito</title>
+    <title>Proyecto 1 - Carrito</title>
 
     <!--Bootstrap 4-->
     <!--link rel="stylesheet" href="assets/css/bootstrap.min.css"-->
-    <link rel="stylesheet" href="assets/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css"> 
 
 
     <!-- Font awesome-->
-    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
 
 
     <!--  WaitMe   -->
     <link rel="stylesheet" href="assets/plugins/waitMe/waitMe.min.css">
 </head>
 <body>
-    <h1>Titulo Modificado </h1>
+
+    <!-- Barra de navegacion -->
+    <div class="w-100">
+    
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarColor02">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#"> <i class="fas fa-shopping-cart"></i> Carritos <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Tienda</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Carrito</a>
+                    </li>
+                </ul>
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="text" placeholder="Buscar..">
+                    <button class="btn btn-secondary my-2 my-sm-0" type="submit">Buscar</button>
+                </form>
+            </div>
+        </nav>
+    </div>
+    <!-- Fin de la Barra de navegacion -->
     
 
+    <!-- Contenido -->
+    <div class="container">
+        <!-- Contenido del sitio -->
+    </div>
+    <!-- Fin del Contenido -->
 
-    <!-- Bootstrap 4-->
-    <script src="assets/js/bootstrap.min.js"></script>
+
+    <!--  Footer --->
+    <div class="container-fluid bg-light py-5">
+        <footer id="footer">
+            <div class="row">
+                <div class="col-xl-4">
+                    <ul class="list-unstyled">
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">RSS</a></li>
+                        <li><a href="#">Twitter</a></li>
+                        <li><a href="#">GitHub</a></li>
+                    </ul>
+                </div>
+                <div class="col-xl-4">
+                    <ul class="list-unstyled">
+                        <li><a href="#">Tienda</a></li>
+                        <li><a href="#">Carrito</a></li>
+                        <li><a href="#">Terminos y condiciones</a></li>
+                    </ul>
+                </div>
+                <div class="col-xl-4 text-right">
+                    <p>Desarrollado por <a href="#">Ricardo Melida</a>.</p>
+                </div>
+            </div>
+        </footer>
+    </div>
+    <!-- Fin del Footer --->
+
 
     <!-- JQuery 3.4.1 -->
-    <script src="assets/js/jquery-3.4.1.slim.min.js"></script>
-
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    
     <!--  Popper JS-->
-    <script src="assets/js/popper.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        
+    <!-- Bootstrap 4-->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
 
     <!--  Sweet Alert 2-->
     <script src="assets/js/sweetalert2.all.js"></script>
@@ -38,8 +101,6 @@
     <!--  WaitMe   -->
     <script src="assets/plugins/waitMe/waitMe.min.js"></script>
 
-    <script>
-      
-    </script>
+
 </body>
 </html>
