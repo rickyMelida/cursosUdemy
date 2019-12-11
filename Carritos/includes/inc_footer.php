4d311983df60793472b09d6fdd74fefc4bf45bb7
Ricardo Mélida <ricardomelida92@gmail.com>
2019-12-11 17:31:54 -0300

    Segmentando la maqueta en diferentes archivos

---
+++ Carritos/includes/inc_footer.php
@@ -0,0 +1,51 @@
+<!--  Footer --->
+<div class="container-fluid bg-light py-5">
+    <footer id="footer">
+        <div class="row">
+            <div class="col-xl-4">
+                <ul class="list-unstyled">
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">RSS</a></li>
+                    <li><a href="#">Twitter</a></li>
+                    <li><a href="#">GitHub</a></li>
+                </ul>
+            </div>
+            <div class="col-xl-4">
+                <ul class="list-unstyled">
+                    <li><a href="#">Tienda</a></li>
+                    <li><a href="#">Carrito</a></li>
+                    <li><a href="#">Terminos y condiciones</a></li>
+                </ul>
+            </div>
+            <div class="col-xl-4 text-right">
+                <p>Desarrollado por <a href="#">Ricardo Melida</a>.</p>
+            </div>
+        </div>
+    </footer>
+</div>
+<!-- Fin del Footer --->
+
+
+    <!-- JQuery 3.4.1 -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    
+    <!--  Popper JS-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        
+    <!-- Bootstrap 4-->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
+    <!--  Sweet Alert 2-->
+    <script src="assets/js/sweetalert2.all.js"></script>
+
+    <!--  WaitMe   -->
+    <script src="assets/plugins/waitMe/waitMe.min.js"></script>
+
+    <script>
+        $(function () {
+            $('[data-toggle="tooltip"]').tooltip()
+        })
+    </script>
+</body>
+</html>
---
+++ Carritos/includes/inc_header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Proyecto 1 - Carrito</title>
+
+    <!--Bootstrap 4-->
+    <!--link rel="stylesheet" href="assets/css/bootstrap.min.css"-->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css"> 
+
+
+    <!-- Font awesome-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+
+
+    <!--  WaitMe   -->
+    <link rel="stylesheet" href="assets/plugins/waitMe/waitMe.min.css">
+</head>
+<body>
+    
