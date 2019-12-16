93ba0b9c445800467e099a7e17fd3a54f890f9b4
Ricardo Mélida <ricardomelida92@gmail.com>
2019-12-15 21:47:10 -0300

    Se agrega archivo products.php

---
+++ Carritos/app/products.php
@@ -0,0 +1,97 @@
+<?php
+
+    //id
+    //sku
+    //nombre
+    //imagen
+    //precio
+    $productos = 
+    [
+        [
+            'id' => 1,
+            'sku' => 456125,
+            'nombre' => 'Playera c306 cuello v | Gris',
+            'imagen' => '01.jpg',
+            'precio' => 61.50
+        ],
+        [
+            'id' => 2,
+            'sku' => 123456,
+            'nombre' => 'Playera c306 Bisson | Blanco',
+            'imagen' => '02.jpg',
+            'precio' => 61.50
+        ],
+        [
+            'id' => 3,
+            'sku' => 789456,
+            'nombre' => 'Playera c306 cuello v | Negro',
+            'imagen' => '03.jpg',
+            'precio' => 45.50
+        ],
+        [
+            'id' => 4,
+            'sku' => 756415,
+            'nombre' => 'Playera d306 Burrito Ortega | Blanco',
+            'imagen' => '04.jpg',
+            'precio' => 61.50
+        ],
+        [
+            'id' => 5,
+            'sku' => 864159,
+            'nombre' => 'Playera d306 mapa mundi | Gris',
+            'imagen' => '05.jpg',
+            'precio' => 31.50
+        ],
+        [
+            'id' => 6,
+            'sku' => 456158,
+            'nombre' => 'Playera d306 cuello v | Blnco',
+            'imagen' => '06.jpg',
+            'precio' => 51.40
+        ],
+        [
+            'id' => 7,
+            'sku' => 4568152,
+            'nombre' => 'Libro proteccion al comsumidor',
+            'imagen' => '07.jpg',
+            'precio' => 21.20
+        ],
+        [
+            'id' => 8,
+            'sku' => 943465,
+            'nombre' => 'Libro El anarquismo argentino',
+            'imagen' => '08.jpg',
+            'precio' => 11.20
+        ],
+        [
+            'id' => 9,
+            'sku' => 369852,
+            'nombre' => 'Libro Gray | E.L. James',
+            'imagen' => '09.jpg',
+            'precio' => 10.50
+        ],
+        [
+            'id' => 10,
+            'sku' => 147852,
+            'nombre' => 'Libro EL Secreto de una mente millonaria',
+            'imagen' => '10.jpg',
+            'precio' => 31.20
+        ],
+        [
+            'id' => 11,
+            'sku' => 3654125,
+            'nombre' => 'Libro Cometas del cielo',
+            'imagen' => '11.jpg',
+            'precio' => 15.20
+        ],
+        [
+            'id' => 12,
+            'sku' => 159654,
+            'nombre' => 'Libro El camino con los sabios',
+            'imagen' => '12.jpg',
+            'precio' => 18.25
+        ],
+    ];
+
+
+    return $productos;
