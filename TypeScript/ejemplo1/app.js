061ed06d3eb1cfc6b1b9475dd8a467c43692fa5f
Ricardo Mélida <ricardomelida92@gmail.com>
2020-02-18 08:02:09 -0300

    Se movieron archivos en carpetas

--- TypeScript/ejemplo1/app.js
+++
@@ -1,9 +0,0 @@
-console.log("Hola Bienvenido");
-console.log('Esto es de TypeScript');
-console.log('Otro mensaje desde TS');
-//Para mostrar advertencias
-console.warn('Por favor cerrar ventana');
-//Parar mostrar un error
-console.error('Error al escribir');
-//Limpiamos la consola
-console.clear();
--- TypeScript/ejemplo1/variables.js
+++
@@ -1,8 +0,0 @@
-var edad, puntos;
-var a = 19;
-edad = 52;
-puntos = a + edad;
-var nombre = 'Ricardo';
-var apellido = ' Melida';
-nombre = nombre + apellido;
-console.log(nombre);
